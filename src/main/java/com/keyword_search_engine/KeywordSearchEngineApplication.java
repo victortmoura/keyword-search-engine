c736e50871ff118e3c33d51391c21c744d5b7511
Victor Moura <victorttmoura@gmail.com>
2025-02-02 19:18:57 -0300

    feat: initial commit - setup project

---
+++ src/main/java/com/keyword_search_engine/KeywordSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.keyword_search_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeywordSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeywordSearchEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keyword_search_engine/KeywordSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyword_search_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeywordSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
